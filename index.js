d34e691043daf102b36d58202cb1a76805ff5d20
박혜민 <hyeminpak@gmail.com>
2023-06-30 23:49:51 +0900

    사용자가 입력한 문자 가져오는 기능구현

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 "use strict";
+const formBtn = document.querySelector(".form__btn");
+formBtn === null || formBtn === void 0 ? void 0 : formBtn.addEventListener("click", () => {
+    const inputTxt = getValue();
+});
+function getValue() {
+    const inputTag = document.querySelector(".form__input");
+    if (inputTag instanceof HTMLInputElement) {
+        return inputTag.value;
+    }
+    else {
+        throw new Error("데이터없음");
+    }
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
+const formBtn = document.querySelector(".form__btn");
 
+formBtn?.addEventListener("click", () => {
+  const inputTxt = getValue();
+});
+
+function getValue(): string {
+  const inputTag = document.querySelector(".form__input");
+  if (inputTag instanceof HTMLInputElement) {
+    return inputTag.value;
+  } else {
+    throw new Error("데이터없음");
+  }
+}
