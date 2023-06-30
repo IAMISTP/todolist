5407d34c9b10c77be54f0989b2424ab926be450a
박혜민 <hyeminpak@gmail.com>
2023-07-01 00:20:50 +0900

    입력한 문자 리스트에 텍스트추가 기능구현

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 "use strict";
 const formBtn = document.querySelector(".form__btn");
+const ul = document.querySelector(".section__ul");
+let count = 0;
 formBtn === null || formBtn === void 0 ? void 0 : formBtn.addEventListener("click", () => {
-    const inputTxt = getValue();
+    let inputTxt = getValue();
+    const newTag = addTag(inputTxt);
+    ul === null || ul === void 0 ? void 0 : ul.appendChild(newTag);
+    count++;
 });
 function getValue() {
     const inputTag = document.querySelector(".form__input");
     if (inputTag instanceof HTMLInputElement) {
-        return inputTag.value;
+        const result = inputTag.value;
+        inputTag.value = "";
+        return result;
     }
     else {
         throw new Error("데이터없음");
     }
+}
+function addTag(txt) {
+    const li = document.createElement("li");
+    li.className = `section__li li_${count}`;
+    li.innerHTML = `
+  <span class="span span_${count}">${txt}</span>
+  <input type="checkbox" class="checkbox_${count}" />
+  <button class="button_${count}"><i class="fa-solid fa-trash"></i></button>
+</li>
+  `;
+    return li;
 }
--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
 const formBtn = document.querySelector(".form__btn");
-
+const ul = document.querySelector(".section__ul");
+let count: number = 0;
 formBtn?.addEventListener("click", () => {
-  const inputTxt = getValue();
+  let inputTxt = getValue();
+  const newTag = addTag(inputTxt);
+  ul?.appendChild(newTag);
+  count++;
 });
 
 function getValue(): string {
   const inputTag = document.querySelector(".form__input");
   if (inputTag instanceof HTMLInputElement) {
-    return inputTag.value;
+    const result = inputTag.value;
+    inputTag.value = "";
+    return result;
   } else {
     throw new Error("데이터없음");
   }
+}
+
+function addTag(txt: string): HTMLLIElement {
+  const li = document.createElement("li");
+  li.className = `section__li li_${count}`;
+  li.innerHTML = `
+  <span class="span span_${count}">${txt}</span>
+  <input type="checkbox" class="checkbox_${count}" />
+  <button class="button_${count}"><i class="fa-solid fa-trash"></i></button>
+</li>
+  `;
+  return li;
 }
