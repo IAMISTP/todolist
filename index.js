3193d84caafd0d5352464248958145ee712a3e94
박혜민 <hyeminpak@gmail.com>
2023-07-01 12:20:04 +0900

    input 에 문자를 입력하지 않으면 todolist에 추가되지 않게 기능구현

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 "use strict";
 const formBtn = document.querySelector(".form__btn");
-const ul = document.querySelector(".section__ul");
-let count = 0;
 formBtn === null || formBtn === void 0 ? void 0 : formBtn.addEventListener("click", () => {
-    let inputTxt = getValue();
-    const newTag = addTag(inputTxt);
-    ul === null || ul === void 0 ? void 0 : ul.appendChild(newTag);
-    count++;
+    const ul = document.querySelector(".section__ul");
+    let inputTxt = getInputValue();
+    if (typeof inputTxt === "string") {
+        const item = createItem(inputTxt);
+        ul === null || ul === void 0 ? void 0 : ul.appendChild(item);
+    }
 });
-function getValue() {
+function getInputValue() {
     const inputTag = document.querySelector(".form__input");
     if (inputTag instanceof HTMLInputElement) {
-        const result = inputTag.value;
-        inputTag.value = "";
-        return result;
-    }
-    else {
-        throw new Error("데이터없음");
+        if (inputTag.value === "") {
+            inputTag.focus();
+        }
+        else {
+            return inputTag.value;
+        }
     }
 }
-function addTag(txt) {
-    const li = document.createElement("li");
-    li.className = `section__li li_${count}`;
-    li.innerHTML = `
-  <span class="span span_${count}">${txt}</span>
-  <input type="checkbox" class="checkbox_${count}" />
-  <button class="button_${count}"><i class="fa-solid fa-trash"></i></button>
-</li>
-  `;
-    return li;
+let id = 0;
+function createItem(text) {
+    const itemRow = document.createElement("li");
+    itemRow.setAttribute("class", "section__li");
+    itemRow.setAttribute("data-id", id.toString());
+    itemRow.innerHTML = `
+  <div class="item" data-id=${id}>
+    <span class="item__name">${text}</span>
+    <button class="item__delete">
+      <i class="fa-solid fa-trash" data-id=${id}></i>
+    </button>
+  </div>
+  <div class="item__divider"></div>
+    `;
+    id++;
+    return itemRow;
 }
--- index.ts
+++ index.ts
@@ -1,32 +1,40 @@
 const formBtn = document.querySelector(".form__btn");
-const ul = document.querySelector(".section__ul");
-let count: number = 0;
+
 formBtn?.addEventListener("click", () => {
-  let inputTxt = getValue();
-  const newTag = addTag(inputTxt);
-  ul?.appendChild(newTag);
-  count++;
+  const ul = document.querySelector(".section__ul");
+
+  let inputTxt = getInputValue();
+  if (typeof inputTxt === "string") {
+    const item = createItem(inputTxt);
+    ul?.appendChild(item);
+  }
 });
 
-function getValue(): string {
+function getInputValue(): string | undefined {
   const inputTag = document.querySelector(".form__input");
   if (inputTag instanceof HTMLInputElement) {
-    const result = inputTag.value;
-    inputTag.value = "";
-    return result;
-  } else {
-    throw new Error("데이터없음");
+    if (inputTag.value === "") {
+      inputTag.focus();
+    } else {
+      return inputTag.value;
+    }
   }
 }
+let id: number = 0;
 
-function addTag(txt: string): HTMLLIElement {
-  const li = document.createElement("li");
-  li.className = `section__li li_${count}`;
-  li.innerHTML = `
-  <span class="span span_${count}">${txt}</span>
-  <input type="checkbox" class="checkbox_${count}" />
-  <button class="button_${count}"><i class="fa-solid fa-trash"></i></button>
-</li>
-  `;
-  return li;
+function createItem(text: string): HTMLLIElement {
+  const itemRow = document.createElement("li");
+  itemRow.setAttribute("class", "section__li");
+  itemRow.setAttribute("data-id", id.toString());
+  itemRow.innerHTML = `
+  <div class="item" data-id=${id}>
+    <span class="item__name">${text}</span>
+    <button class="item__delete">
+      <i class="fa-solid fa-trash" data-id=${id}></i>
+    </button>
+  </div>
+  <div class="item__divider"></div>
+    `;
+  id++;
+  return itemRow;
 }
